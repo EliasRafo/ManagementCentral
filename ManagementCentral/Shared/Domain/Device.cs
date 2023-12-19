9a12134c49a4076944b92ab736282821c814b710
Elias Rafo <rafoelias@hotmail.com>
2023-12-19 01:08:21 +0100

    Add project files.

---
+++ ManagementCentral/Shared/Domain/Device.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementCentral.Shared.Domain
+{
+    public enum Status
+    {
+        online,
+        offline
+    }
+
+    public enum Location
+    {
+        Sweden,
+        England
+    }
+    public class Device
+    {
+        public int DeviceId { get; set; }
+
+        public Location Location { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [StringLength(15, ErrorMessage = "Längden måste vara mellan {2} och {1}", MinimumLength = 6)]
+        public string DeviceType { get; set; } = string.Empty;
+
+        public Status Status { get; set; }
+    }
+}
