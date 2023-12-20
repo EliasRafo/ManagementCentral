6d49438a1984fbfee756acad2d8833a20afbdb11
Elias Rafo <rafoelias@hotmail.com>
2023-12-20 09:47:09 +0100

    Add ServiceApi

--- ManagementCentral/Client/Program.cs
+++ ManagementCentral/Client/Program.cs
@@ -16,6 +16,9 @@ builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().Cre
 
 builder.Services.AddApiAuthorization();
 
+var apiBaseAddress = "https://localhost:7150";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseAddress) });
+
 builder.Services.AddSingleton<IDeviceDataService, DeviceDataService>();
 
 await builder.Build().RunAsync();
---
+++ ServiceApi/Program.cs
@@ -0,0 +1,84 @@
+using ManagementCentral.Shared.Domain;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var specOrigin = "MySpecOrigin";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: specOrigin, policy =>
+    {
+        policy.WithOrigins("https://localhost:7084")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+// device
+app.MapGet("/device", () => "Getting a device from API");
+
+app.MapGet("/device/{deviceId}/button/{buttonId}",
+    (int deviceId, int buttonId) => $"Deviceid {deviceId} and ButtonId {buttonId}");
+
+app.MapGet("/device/{deviceId}", (int deviceId) =>
+{
+    var DeviceService = new DeviceService();
+    return DeviceService.DeviceList[deviceId].DeviceType;
+});
+
+app.UseCors(specOrigin);
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+class DeviceService
+{
+    public List<Device> DeviceList { get; set; } = new List<Device>();
+
+    public DeviceService()
+    {
+        DeviceList.Add(new Device() { DeviceId = 1, Location = Location.Sweden, Date = DateTime.Now, DeviceType = "Sensor", Status = Status.online });
+        DeviceList.Add(new Device() { DeviceId = 2, Location = Location.England, Date = DateTime.Now.AddDays(-30), DeviceType = "Machine", Status = Status.offline });
+    }
+}
